397112f729ed62547c19c4248f7860b1a63a13db
EhwazM <127918270+EhwazM@users.noreply.github.com>
2025-05-05 23:26:02 -0500

    Update

--- physics/linear_regression.py
+++ physics/linear_regression.py
@@ -60,7 +60,6 @@ class DGRegression:
 
         for epoch in range(self.epochs):
             if (self.lil_batch is None and self.seed is None ):
-                #eta = self.aprendizaje(i, self.eta0) #Tasa de aprendizaje
                 grad = (2/m)*(x_b.T.dot(x_b.dot(self.theta) - y))
                     
             elif (self.lil_batch is not None and self.seed is None):
@@ -68,9 +67,9 @@ class DGRegression:
                 x_bm = x_b[index_m]
                 y_m = y[index_m]
 
-                for j in range(0, m, self.mini_lote):
-                    xi = x_bm[j:j + self.mini_lote]
-                    yi = y_m[j:j + self.mini_lote]
+                for j in range(0, m, self.lil_batch):
+                    xi = x_bm[j:j + self.lil_batch]
+                    yi = y_m[j:j + self.lil_batch]
 
                     eta = self.eta0
                     grad = (2/self.lil_batch)*(xi.T.dot(xi.dot(self.theta)-yi))
@@ -112,7 +111,8 @@ class Ridge():
     def fit(self, x, y):
         m, n = x.shape
         x_b = np.c_[np.ones((m,1)), x]
-        I = np.identity(n+1)
+        I = np.identity(n + 1)
+        I[0, 0] = 0
         self.theta = np.linalg.inv(x_b.T.dot(x_b) + self.alpha*I).dot(x_b.T).dot(y)
         self.intercept = self.theta[0]
         self.coef = self.theta[1:]
@@ -123,8 +123,8 @@ class Ridge():
         y_pred = x_b.dot(self.theta)
         return y_pred
 
-class Lasso():
-    def __init__(self, alpha = 0.1, epochs = 1000, lil_batch = None, seed = None, eta0 = 0.1, pacience = 10, tol = 1e-3):
+class Lasso:
+    def __init__(self, alpha=0.1, epochs=1000, lil_batch=None, seed=None, eta0=0.1, pacience=10, tol=1e-3):
         self.alpha = alpha
         self.epochs = epochs
         self.lil_batch = lil_batch
@@ -137,7 +137,7 @@ class Lasso():
         self.coef = None
 
     def learn(self, epoch, eta0):
-        return(eta0/(1 + epoch))
+        return eta0 / (1 + epoch)
 
     def fit(self, x, y):
         m, n = x.shape
@@ -145,62 +145,111 @@ class Lasso():
         self.theta = np.random.rand(n+1, 1)
         best_error = float("inf")
         pacience_counter = 0
-        
+
         for epoch in range(self.epochs):
             theta_p = self.theta.copy()
 
-            if (self.lil_batch is None and self.seed is None ):
-                for i in range(n):
-                    eta = self.learn(i, self.eta0) #Tasa de aprendizaje
-                    if i == 0:
-                        grad = (2/m)*(x_b.T.dot(x_b.dot(self.theta) - y))
-                        
-                    else:
-                        grad = (2/m)*(x_b.T.dot(x_b.dot(self.theta) - y + self.alpha*np.sign(self.theta[i])))
+            grad = (2/m) * x_b.T.dot(x_b.dot(self.theta) - y)
+            grad += self.alpha * np.sign(self.theta) 
 
-            elif (self.lil_batch is not None and self.seed is None):
-                index_m = np.random.permutation(m)
-                x_bm = x_b[index_m]
-                y_m = y[index_m]
-
-                for j in range(0, m, self.lil_batch):
-                    xi = x_bm[j:j + self.lil_batch]
-                    yi = y_m[j:j + self.lil_batch]
-
-                    for i in range(n):
-                        eta = self.learn(i, self.eta0) #Tasa de aprendizaje
-    
-                        if i == 0:
-                            grad = (2/self.lil_batch)*(xi.T.dot(xi.dot(self.theta) - yi))
-    
-                        else:
-                            grad = (2/self.lil_batch)*(xi.T.dot(xi.dot(self.theta) - yi + self.alpha*np.sign(self.theta[i])))
-
-
-            elif (self.lil_batch is None and self.seed is not None):
-                for j in range(m):
-                    random_index = np.random.randint(m)
-                    xi = x_b[random_index:random_index + 1]
-                    yi = y[random_index:random_index + 1]
-                    
-                    for i in range(n):
-                        eta = self.learn(i, self.eta0) #Tasa de aprendizaje
-
-                        if i == 0:
-                            grad = 2*(xi.T.dot(xi.dot(self.theta) - yi))
-    
-                        else:
-                            grad = 2*(xi.T.dot(xi.dot(self.theta) - yi + self.alpha*np.sign(self.theta[i])))
-
-                
-            if(np.linalg.norm(self.theta - theta_p) < self.tol):
-                break
-                    
-            self.theta -= self.learn(epoch, self.eta0)*grad
+            self.theta -= self.learn(epoch, self.eta0) * grad
             self.intercept = self.theta[0]
             self.coef = self.theta[1:]
+
+            currently_error = self.error_calc(x_b, y)
+
+            if np.linalg.norm(self.theta - theta_p) < self.tol:
+                print(f"Se detuvo por convergencia en iteración {epoch + 1}")
+                break
+
+            if currently_error < best_error:
+                best_error = currently_error
+                pacience_counter = 0
+            else:
+                pacience_counter += 1
+
+            if pacience_counter >= self.pacience:
+                print(f"Se detuvo por detención anticipada en iteración {epoch + 1}")
+                break
+# class Lasso():
+#     def __init__(self, alpha = 0.1, epochs = 1000, lil_batch = None, seed = None, eta0 = 0.1, pacience = 10, tol = 1e-3):
+#         self.alpha = alpha
+#         self.epochs = epochs
+#         self.lil_batch = lil_batch
+#         self.seed = seed
+#         self.eta0 = eta0
+#         self.pacience = pacience
+#         self.tol = tol
+#         self.theta = None
+#         self.intercept = None
+#         self.coef = None
+
+#     def learn(self, epoch, eta0):
+#         return(eta0/(1 + epoch))
+
+#     def fit(self, x, y):
+#         m, n = x.shape
+#         x_b = np.c_[np.ones((m,1)), x]
+#         self.theta = np.random.rand(n+1, 1)
+#         best_error = float("inf")
+#         pacience_counter = 0
+        
+#         for epoch in range(self.epochs):
+#             theta_p = self.theta.copy()
+
+#             if (self.lil_batch is None and self.seed is None ):
+#                 for i in range(n):
+#                     eta = self.learn(i, self.eta0) #Tasa de aprendizaje
+#                     if i == 0:
+#                         grad = (2/m)*(x_b.T.dot(x_b.dot(self.theta) - y))
+                        
+#                     else:
+#                         grad = (2/m) * x_b.T.dot(x_b.dot(self.theta) - y)
+#                         grad += self.alpha * np.sign(self.theta)  # fuera del dot
+
+#             elif (self.lil_batch is not None and self.seed is None):
+#                 index_m = np.random.permutation(m)
+#                 x_bm = x_b[index_m]
+#                 y_m = y[index_m]
+
+#                 for j in range(0, m, self.lil_batch):
+#                     xi = x_bm[j:j + self.lil_batch]
+#                     yi = y_m[j:j + self.lil_batch]
+
+#                     for i in range(n):
+#                         eta = self.learn(i, self.eta0) #Tasa de aprendizaje
+    
+#                         if i == 0:
+#                             grad = (2/self.lil_batch)*(xi.T.dot(xi.dot(self.theta) - yi))
+    
+#                         else:
+#                             grad = (2/self.lil_batch)*(xi.T.dot(xi.dot(self.theta) - yi + self.alpha*np.sign(self.theta[i])))
+
+
+#             elif (self.lil_batch is None and self.seed is not None):
+#                 for j in range(m):
+#                     random_index = np.random.randint(m)
+#                     xi = x_b[random_index:random_index + 1]
+#                     yi = y[random_index:random_index + 1]
+                    
+#                     for i in range(n):
+#                         eta = self.learn(i, self.eta0) #Tasa de aprendizaje
+
+#                         if i == 0:
+#                             grad = 2*(xi.T.dot(xi.dot(self.theta) - yi))
+    
+#                         else:
+#                             grad = 2*(xi.T.dot(xi.dot(self.theta) - yi + self.alpha*np.sign(self.theta[i])))
+
+                
+#             if(np.linalg.norm(self.theta - theta_p) < self.tol):
+#                 break
+                    
+#             self.theta -= self.learn(epoch, self.eta0)*grad
+#             self.intercept = self.theta[0]
+#             self.coef = self.theta[1:]
             
-            currenly_error = self.error_calc(x_b, y)
+#             currenly_error = self.error_calc(x_b, y)
 
 
     def predict(self, x):
